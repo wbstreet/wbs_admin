184f20fcf1e270fd3891bae31c9f8c8f6a0509e0
Konstantin Polyakov <kostyan_93@mail.ru>
2017-12-13 09:20:01 +0300

    deleted code for including Twig

--- lib.class.admin.php
+++ lib.class.admin.php
@@ -13,24 +13,8 @@
 
  */
 
-
-
-
-
-$path_twig = WB_PATH.'/include/Sensio/Twig/lib/Twig/Autoloader.php';
-
-if (file_exists($path_twig)) {
-
-    if (!class_exists('Twig_Environment')) include($path_twig);
-
-} else echo "<script>console.log('Модуль минимаркета требует шаблонизатор Twig')</script>";
-
-
-
 $path_core = WB_PATH.'/modules/wbs_core/include_all.php';
-
 if (file_exists($path_core)) include($path_core);
-
 else echo "<script>console.log('Модуль минимаркета требует модуль \"wbs_core\"')</script>";
 
 
@@ -38,14 +22,9 @@ else echo "<script>console.log('Модуль минимаркета требуе
 class WbsAdmin extends Addon {
 
     function __construct() {
-
         parent::__construct('wbs_admin', null, null);
     }
 
-    
-
 }
-
-
 
 ?>
